9bbaa67a8275a2163df0112a971d96c52a752463
Marcelo Andrade <meandrade@localhost.(none)>
2013-02-07 21:15:44 -0200

    Fixing AJAX problems

--- public/toaster/app/app_model.js
+++ public/toaster/app/app_model.js
@@ -10,54 +10,6 @@
       return AppModel.__super__.constructor.apply(this, arguments);
     }
 
-    AppModel._request = function(method, url, data) {
-      var fetcher, req,
-        _this = this;
-      if (data == null) {
-        data = '';
-      }
-      fetcher = new theoricus.mvc.lib.Fetcher;
-      req = $.ajax({
-        url: url,
-        type: method,
-        data: data,
-        async: false
-      });
-      req.done(function(data) {
-        fetcher.loaded = true;
-        fetcher.records = _this._instantiate(data);
-        return typeof fetcher.onload === "function" ? fetcher.onload(fetcher.records) : void 0;
-      });
-      req.error(function(error) {
-        fetcher.error = true;
-        if (fetcher.onerror != null) {
-          return fetcher.onerror(error);
-        } else {
-          throw error;
-        }
-      });
-      return fetcher;
-    };
-
-    AppModel._instantiate = function(data) {
-      var Factory, classname, model, obj, records, _collection;
-      Factory = theoricus.core.Factory;
-      classname = (("" + this).match(/function\s(\w+)/))[1];
-      records = [];
-      obj = {};
-      $.map(data, function(value, key) {
-        return obj[key] = value;
-      });
-      model = Factory.model(classname, obj);
-      records.push(model);
-      _collection = records;
-      if (records.length === 1) {
-        return records[0];
-      } else {
-        return records;
-      }
-    };
-
     return AppModel;
 
   })(theoricus.mvc.Model);
